ab3c862a79cffe159b952fa227b83b39c421e56b
Joe Duran <jdduran@Amarillocollege.com>
2025-03-26 15:37:58 -0500

    first commit

--- project02-01.js
+++ project02-01.js
@@ -3,9 +3,31 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Joe Duran
+      Date:   3/26/2025
 
       Filename: project02-01.js
  */
 
+function FahrenheitToCelsius (degree)
+{
+      return (degree-32)/1.8;
+
+}
+
+function CelsiusToFahrenheit (degree)
+{
+      return degree *1.8 + 32;
+}
+
+document.getElementById("cvalue").onchange = function ()
+{
+let cDegree = document.getElementById("cvalue").value;
+document.getElementById("fValue").value=CelsiusToFahrenhei (cDegree);
+}
+
+document.getElementById("fvalue").onchange = function ()
+{
+let fDegree = document.getElementById("fvalue").value;
+document.getElementById("cValue").value=FahrenheitToCelsius (fDegree);
+}
